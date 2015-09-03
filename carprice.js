e05bd2619b461068896a073a6c3364c6b31a83a6
Vlad Mehakovic <vlad.mehakovic@X-Vlad-Mehakovic-MBP13.local>
2015-09-04 08:03:18 +1000

    Updated css

--- carprice.js
+++ carprice.js
@@ -3,6 +3,10 @@ if (Meteor.isClient) {
   Template.all.helpers({
     cars: function() {
       return Cars.find();
+    },
+    currencyPrice: function() {
+      var price = this.price;
+      return '$' + price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
   });
 }
